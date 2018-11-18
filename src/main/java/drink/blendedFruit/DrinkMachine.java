034cf5b514d46c64953ec98d3767101abccb5e81
Edgar Diaz <diaz_eda@hotmail.com>
2018-11-18 12:47:20 -0500

    resolved skill test

---
+++ src/main/java/drink/blendedFruit/DrinkMachine.java
@@ -0,0 +1,71 @@
+package drink.blendedFruit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DrinkMachine {
+
+	private static Map<Ingredient, Integer> stock = new HashMap<>();
+
+	public static void main(String[] args) {
+
+		System.out.println("q-> Exit	r-> Restock		m->Menu 	i->Inventory");
+
+		DrinkMachine machine = new DrinkMachine();
+		DrinkMachineUtil.getInstance().restock(stock);
+		DrinkMachineUtil.getInstance().getInventory(stock);
+		DrinkMachineUtil.getInstance().getMenu(stock);
+		machine.startIO();
+	}
+
+	public void startIO() {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			System.out.print("\nChoose an option from the menu (drink,size): \n");
+			try {
+				String[] inputs = reader.readLine().toLowerCase().split(",");
+
+				switch (inputs[0].trim()) {
+				case "":
+					break;
+				case "q":
+					System.exit(0);
+					break;
+				case "r":
+					DrinkMachineUtil.getInstance().restock(stock);
+					System.out.println("Restock!!!! ");
+					break;
+				case "m":
+					DrinkMachineUtil.getInstance().getMenu(stock);
+					break;
+				case "i":
+					DrinkMachineUtil.getInstance().getInventory(stock);
+					break;
+
+				default:
+					DrinkMachineUtil.getInstance().make(stock, inputs);
+					break;
+				}
+
+			} catch (OutOfStockException e) {
+				System.out.println("Please restock!!!! ");
+			} catch (NumberFormatException e) {
+				System.out.println("XXX  Invalid selection");
+			} catch (IOException e) {
+				System.out.println("XXX  Invalid input");
+			}
+		}
+	}
+
+	@SuppressWarnings("serial")
+	public static class OutOfStockException extends RuntimeException {
+		public OutOfStockException(Drink drink) {
+			super(drink.toString());
+		}
+	}
+}
---
+++ src/main/java/drink/blendedFruit/Ingredient.java
@@ -0,0 +1,29 @@
+package drink.blendedFruit;
+
+import java.math.BigDecimal;
+
+public enum Ingredient {
+	
+	STRAWBERRY_G("0.04"), 
+	BANANA_G("0.04"), 
+	MANGO_G("0.045"), 
+	CONDENSED_MILK_ML("0.02"), 
+	ICE_ML("0.015"), 
+	SUGAR_G("0.010");
+
+	private final String name;
+	private final BigDecimal cost;
+
+	Ingredient(String cost) {
+		this.name = this.name().replace("_", " ").toLowerCase();
+		this.cost = new BigDecimal(cost);
+	}
+
+	public BigDecimal getCost() {
+		return this.cost;
+	}
+
+	public String toString() {
+		return this.name;
+	}
+}
